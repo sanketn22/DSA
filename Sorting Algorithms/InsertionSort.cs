9a8e9eb6283a5ef0db820e99c0eeffcd4158c2f3
sanket <narvekarsanket63@gmail.com>
2025-06-02 21:55:04 +0530

    Insertion Sort

--- Sorting Algorithms/InsertionSort.cs
+++ Sorting Algorithms/InsertionSort.cs
@@ -8,12 +8,27 @@ namespace DSA.Sorting_Algorithms
 {
     public class InsertionSort
     {
-
-        public void sort(int[] nums)
+        /*
+         * take an element and place it in its correct position
+         * checks the first element and consider it as in its correct position
+         * increase the window and start checking from the last element of the window if they are in the correct postion.
+         * if not, swap with the adjacent element till they are in the correct position
+         * 
+         * eg. [2,4,1,5,3]
+         */
+        public void sort(int[] arr)
         {
-            for (int i = 0; i < nums.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-
+                int j = i;
+                
+                while (j>0 && arr[j-1]>arr[j])
+                {
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                    j--;
+                }
             }
         }
        
